ae44dfc86c5e2c26cacc7a827caa2e23db1c385e
painye <1850384745@qq.com>
2021-03-03 21:50:43 +0800

    已经测试过将counter模块中的who方法进行了fegin的集群，均衡负载

---
+++ counter/src/main/java/com/yp/bank/counter/controller/AccountController.java
@@ -0,0 +1,71 @@
+package com.yp.bank.counter.controller;
+/**
+ * @author pan
+ * @date 2021/3/2 15:35
+ */
+
+import com.yp.bank.counter.service.IAccountService;
+import com.yp.bank.common.dto.HttpResp;
+import com.yp.bank.common.entity.Account;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @ClassName : com.yp.bank.counter.controller.AccountMapper
+ * @Description : 类描述
+ * @author pan
+ * @date 2021/3/2 15:35
+ */
+
+@RestController
+@Api(tags = "账户管理模块")
+@RequestMapping("app/bank/counter")
+public class AccountController {
+    @Autowired
+    public IAccountService iAccountService;
+
+    @ApiOperation("办银行卡， 增加账户")
+    @PutMapping("/addAccount.do")
+    public HttpResp addAccount( Account account){
+        iAccountService.addAccount(account);
+        return  new HttpResp(200, "成功插入", null, LocalDateTime.now());
+    }
+
+    @ApiOperation("删除账户， 银行卡注销")
+    @PostMapping("/deleteAccount.do")
+    public HttpResp deleteAccount(int accountId){
+        iAccountService.deleteAccount(accountId);
+        return new HttpResp(200, "成功删除", null, LocalDateTime.now());
+    }
+
+    @ApiOperation("修改账户基本信息")
+    @PostMapping("/updateAccount.do")
+    public HttpResp updateAccount(Account account){
+        iAccountService.updateAccount(account);
+        return new HttpResp(200, null, "成功修改账户信息",LocalDateTime.now());
+    }
+
+    @ApiOperation("根据id查询账户的详细的情况")
+    @GetMapping("/findOneByID.do")
+    public HttpResp findOneById(int userId){
+        Account oneById = iAccountService.findOneById(userId);
+        return new HttpResp(200, "成功查找", oneById, LocalDateTime.now());
+    }
+
+    @ApiOperation("所有账户信息")
+    @GetMapping("/findAll.do")
+    public HttpResp findAll(){
+        List<Account> all = iAccountService.findAll();
+        return new HttpResp(200, "成功查找", all, LocalDateTime.now());
+    }
+
+    @GetMapping("/who.do")
+    public String who(){
+        return "I am counter 1";
+    }
+}
---
+++ counter/src/main/java/com/yp/bank/counter/service/imp/IAccountServiceImpl.java
@@ -0,0 +1,60 @@
+package com.yp.bank.counter.service.imp;
+/**
+ * @author pan
+ * @date 2021/3/2 15:04
+ */
+
+import com.yp.bank.counter.mapper.IAccountMapper;
+import com.yp.bank.counter.service.IAccountService;
+import com.yp.bank.common.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @ClassName : com.yp.bank.counter.service.imp.IAccountServiceImpl
+ * @Description : 类描述
+ * @author pan
+ * @date 2021/3/2 15:04
+ */
+
+@Service
+public class IAccountServiceImpl  implements IAccountService {
+    @Autowired
+    public IAccountMapper accountMapper;
+
+    @Override
+    public int addAccount(Account account) {
+        Date date = new Date();
+        account.setAccountCreateTime(date);
+        account.setAccountDelete(0);
+        return accountMapper.addAccount(account);
+    }
+
+
+    @Override
+    public int deleteAccount(int accountId) {
+        return accountMapper.deleteAccount(accountId);
+    }
+
+    @Override
+    public int updateAccount(Account account) {
+        Date date = new Date();
+        account.setAccountUpdateTime(date);
+        return accountMapper.updateAccount(account);
+    }
+
+    @Override
+    public Account findOneById(int accountId) {
+        return accountMapper.findOneById(accountId);
+    }
+
+
+    @Override
+    public List<Account> findAll() {
+        return accountMapper.findAll();
+    }
+}
